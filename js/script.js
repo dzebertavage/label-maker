9edf10484be0e436b4835bbbe4866fff45f8c608
Devin Zebertavage <60449275+dzebertavage@users.noreply.github.com>
2025-01-08 20:50:25 -0500

    Create logic so sell-by date reflects radio button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-let date = moment();
-let currentDate = date.format(`MM/DD/YYYY`);
+const date = moment();
+let currentDate = date.format(`MM/DD/YY`);
 let currentYear = date.format(`YYYY`);
-let sellByDate = moment().add(3, `months`).calendar();
+let sellByDate = date.add(3, `months`).format("MM/DD/YY");
+let wholeRadio = document.querySelector("#wholeBean");
+let groundRadio = document.querySelector("#ground");
+let sellByDiv = document.querySelector(".sell-by");
 
 const labelMakerContainer = document.querySelector(".label-maker-container");
 const optionsContainer = document.querySelector(".options-container");
@@ -15,3 +18,14 @@ function generateFooterContent() {
 }
 
 generateFooterContent();
+sellByDiv.textContent = sellByDate;
+
+wholeRadio.addEventListener("click", () => {
+    sellByDate = moment().add(3, `months`).format("MM/DD/YY");
+    sellByDiv.textContent = sellByDate;
+});
+
+groundRadio.addEventListener("click", () => {
+    sellByDate = moment().add(1, `month`).format("MM/DD/YY");
+    sellByDiv.textContent = sellByDate;
+});
