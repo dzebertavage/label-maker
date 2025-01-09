e79b758e82a9306ee58d13d7cf0c4fff368a493b
Devin Zebertavage <60449275+dzebertavage@users.noreply.github.com>
2025-01-08 19:48:57 -0500

    Add momentjs, streamline sell-by date manipulation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-const date = new Date();
-const year = date.getFullYear();
+let date = moment();
+let currentDate = date.format(`MM/DD/YYYY`);
+let currentYear = date.format(`YYYY`);
+let sellByDate = moment().add(3, `months`).calendar();
+
 const labelMakerContainer = document.querySelector(".label-maker-container");
 const optionsContainer = document.querySelector(".options-container");
 const labelPreviewContainer = document.querySelector(".label-preview-container");
@@ -7,7 +10,7 @@ const footer = document.querySelector(".footer");
 
 function generateFooterContent() {
     const footerPara = document.createElement("p");
-    footerPara.textContent = `\u00A9 ${year} | Devin Zebertavage`;
+    footerPara.textContent = `\u00A9 ${currentYear} Devin Zebertavage`;
     footer.appendChild(footerPara);
 }
 
